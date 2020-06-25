f7c1fac714f14b75cef5ec5d1191c40bd44c1b93
Numbericons <zachjoliver@gmail.com>
2020-06-24 23:27:09 -0700

    Add working version with 25 iterations

---
+++ lastPass.lua
@@ -0,0 +1,18 @@
+pressReturn = function()
+    hs.eventtap.keyStroke({}, "return")
+end
+
+deleteItem = function()
+    mp = hs.mouse.getAbsolutePosition()
+    hs.eventtap.event.newMouseEvent(1, {x=mp.x, y=mp.y},{"ctrl"}):post()
+    hs.eventtap.event.newMouseEvent(1, {x=mp.x+30, y=mp.y+35}):post()
+    hs.eventtap.event.newMouseEvent(2, {x=mp.x+30, y=mp.y+35}):post()
+    hs.mouse.setAbsolutePosition({x=mp.x, y=mp.y})
+    hs.timer.doAfter(1, pressReturn)
+end
+
+hs.hotkey.bind({"cmd", "alt"}, "Z", function()
+    for i = 1, 25 do
+        hs.timer.doAfter(2*i, deleteItem)
+    end
+end)
